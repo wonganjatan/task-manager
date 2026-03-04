aaab8dffd81bc6d9f2b11ff0a38569aa4864d644
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-05 08:25:42 +1100

    feat(RegistrationDTO): add more attributes

--- src/main/java/com/wonganjatan/taskmanager/model/RegistrationDTO.java
+++ src/main/java/com/wonganjatan/taskmanager/model/RegistrationDTO.java
@@ -6,6 +6,15 @@ import jakarta.validation.constraints.Size;
 public class RegistrationDTO {
 
     @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
     private String username;
 
     @NotBlank
@@ -14,16 +23,30 @@ public class RegistrationDTO {
 
     public RegistrationDTO() {}
 
-    public RegistrationDTO(String username, String password) {
+    public RegistrationDTO(
+            String firstName,
+            String lastName,
+            String email,
+            String username,
+            String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
         this.username = username;
         this.password = password;
     }
 
     // Setter
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setEmail(String email) { this.email = email; }
     public void setUsername(String username) { this.username = username; }
     public void setPassword(String password) { this.password = password; }
 
     // Getter
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getEmail() { return email; }
     public String getUsername() { return username; }
     public String getPassword() { return password; }
 }
