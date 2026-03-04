b4708c76a4559034351dc427743d93b100392154
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-05 08:28:00 +1100

    feat(User): add more attibute to the User class

--- src/main/java/com/wonganjatan/taskmanager/model/User.java
+++ src/main/java/com/wonganjatan/taskmanager/model/User.java
@@ -1,6 +1,7 @@
 package com.wonganjatan.taskmanager.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 
 @Entity
@@ -10,6 +11,15 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotEmpty
+    private String firstName;
+
+    @NotEmpty
+    private String lastName;
+
+    @NotEmpty
+    private String email;
 
     @NotEmpty(message = "Username cannot be empty")
     @Column(unique = true, nullable = false)
@@ -26,11 +36,17 @@ public class User {
     }
 
     // Setter
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setEmail(String email) { this.email = email; }
     public void setUsername(String username) { this.username = username; }
     public void setPassword(String password) { this.password = password; }
 
     // Getter
     public Long getId() { return id; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getEmail() { return email; }
     public String getUsername() { return username; }
     public String getPassword() { return password; }
 }
