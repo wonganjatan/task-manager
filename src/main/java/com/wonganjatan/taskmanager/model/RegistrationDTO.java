d6d84fb058d57c6801c3bf296314033751731eba
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-04 18:58:18 +1100

    feat(RegistrationDTO): add improvement to the class

--- src/main/java/com/wonganjatan/taskmanager/model/RegistrationDTO.java
+++ src/main/java/com/wonganjatan/taskmanager/model/RegistrationDTO.java
@@ -3,7 +3,7 @@ package com.wonganjatan.taskmanager.model;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public class RegistrationForm {
+public class RegistrationDTO {
 
     @NotBlank
     private String username;
@@ -12,5 +12,18 @@ public class RegistrationForm {
     @Size(min = 6)
     private String password;
 
+    public RegistrationDTO() {}
 
+    public RegistrationDTO(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Setter
+    public void setUsername(String username) { this.username = username; }
+    public void setPassword(String password) { this.password = password; }
+
+    // Getter
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
 }
