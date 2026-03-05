6f501ccd834014875a941223744932cf1142eb12
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-05 16:48:31 +1100

    feat: add TaskCreation class

---
+++ src/main/java/com/wonganjatan/taskmanager/model/TaskCreation.java
@@ -0,0 +1,58 @@
+package com.wonganjatan.taskmanager.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+public class TaskCreation {
+
+    @NotBlank(message = "Title cannot be empty")
+    @Size(min = 3, max = 20, message = "Title must be 3-20 characters")
+    private String title;
+
+    @NotBlank
+    @Size(max = 500, message = "Desciption cannot exceeed 500 characters")
+    private String description;
+
+    private String priority;
+    private LocalDateTime createdAt;
+
+    @NotNull(message = "Due date is required")
+    private LocalDateTime dueDate;
+    private boolean isComplete;
+
+    public TaskCreation() {}
+
+    public TaskCreation(
+            String title,
+            String description,
+            String priority,
+            LocalDateTime createdAt,
+            LocalDateTime dueDate,
+            boolean isComplete) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.createdAt = createdAt;
+        this.dueDate = dueDate;
+        this.isComplete = isComplete;
+    }
+
+    // Setter
+    public void setTitle(String title) { this.title = title; }
+    public void setDescription(String description) { this.description = description; }
+    public void setPriority(String priority) { this.priority = priority; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+    public void setDueDate(LocalDateTime dueDate) { this.dueDate = dueDate; }
+    public void setIsComplete(boolean isComplete) { this.isComplete = isComplete; }
+
+    // Getter
+    public String getTitle() { return title; }
+    public String getDescription() { return description; }
+    public String getPriority() { return priority; }
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public LocalDateTime getDueDate() { return dueDate; }
+    public boolean getIsComplete() { return isComplete; }
+}
