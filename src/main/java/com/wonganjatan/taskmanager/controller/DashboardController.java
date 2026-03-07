5b0a3b81603afaa95d95a4b2961296abd7ce364b
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-07 15:40:39 +1100

    feat: add DashboardController class to controll the dashboard page

---
+++ src/main/java/com/wonganjatan/taskmanager/controller/DashboardController.java
@@ -0,0 +1,61 @@
+package com.wonganjatan.taskmanager.controller;
+
+import com.wonganjatan.taskmanager.model.Task;
+import com.wonganjatan.taskmanager.model.TaskCreation;
+import com.wonganjatan.taskmanager.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping("/dashboard")
+public class DashboardController {
+
+    private final TaskService taskService;
+
+    @Autowired
+    public DashboardController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+    
+    @GetMapping
+    public String dashboard(Model model) {
+        Collection<Task> tasks = taskService.getAllTasks();
+        model.addAttribute("tasks", tasks);
+
+        return "dashboard";
+    }
+
+    @GetMapping("/new")
+    public String taskCreationForm(Model model) {
+        if (!model.containsAttribute("taskCreationForm")) {
+            model.addAttribute("taskCreationForm", new TaskCreation());
+        }
+
+        return "new";
+    }
+
+    @PostMapping
+    public String taskCreation(@ModelAttribute("taskCreationForm") TaskCreation form, Model model) {
+        Task newTask = new Task();
+        newTask.setTitle(form.getTitle());
+        newTask.setDescription(form.getDescription());
+        newTask.setPriority(form.getPriority());
+        newTask.setCreatedAt(form.getCreatedAt());
+        newTask.setDueDate(form.getDueDate());
+        newTask.setIsComplete(form.getIsComplete());
+
+        if (taskService.createTask(newTask) != null) {
+            System.out.println("created");
+        }
+
+        return "redirect:/dashboard";
+    }
+
+}
