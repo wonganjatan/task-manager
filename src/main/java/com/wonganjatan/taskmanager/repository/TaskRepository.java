efab935980f0d567c9c7164992ce9e3e590d0bce
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-10 14:24:08 +1100

    fix: remove old isComplete methods and add count method to find all tasks

--- src/main/java/com/wonganjatan/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/wonganjatan/taskmanager/repository/TaskRepository.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    long countByIsCompleteFalse();
-    long countByIsCompleteTrue();
+    long count();
 }
