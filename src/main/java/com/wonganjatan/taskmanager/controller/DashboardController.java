e9b390eae6045a830a2a55f0ca265add77ffb279
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-10 18:23:56 +1100

    fix: change task.getStatus() to task.status

--- src/main/java/com/wonganjatan/taskmanager/controller/DashboardController.java
+++ src/main/java/com/wonganjatan/taskmanager/controller/DashboardController.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.Collection;
 
 @Controller
