d3347682ac7c4f7165d55b1749ac8c4da99c962b
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-15 19:06:40 +1100

    feat: add UserDashboardController

---
+++ src/main/java/com/wonganjatan/taskmanager/controller/user/UserDashboardController.java
@@ -0,0 +1,40 @@
+package com.wonganjatan.taskmanager.controller.user;
+
+import com.wonganjatan.taskmanager.model.User;
+import com.wonganjatan.taskmanager.service.TaskService;
+import com.wonganjatan.taskmanager.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/user")
+public class UserDashboardController {
+
+    private final TaskService taskService;
+    private final UserService userService;
+
+    @Autowired
+    public UserDashboardController(TaskService taskService, UserService userService) {
+        this.taskService = taskService;
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String dashboard(Authentication authentication, Model model) {
+
+        String username = authentication.getName();
+        Optional<User> userOptional = userService.getUserByUsername(username);
+        User user = userOptional.get();
+        long taskCount = taskService.getTaskCountByAssignedUser(user.getId());
+
+        model.addAttribute("taskCount", taskCount);
+
+        return "user/dashboard";
+    }
+}
