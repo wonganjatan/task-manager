e6cec871a4a71f2415a812f6571cc6a06b668af9
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-15 19:13:43 +1100

    feat: add UserTasksCOntroller and view html to let user view task

---
+++ src/main/java/com/wonganjatan/taskmanager/controller/user/UserTasksController.java
@@ -0,0 +1,74 @@
+package com.wonganjatan.taskmanager.controller.user;
+
+import com.wonganjatan.taskmanager.model.Task;
+import com.wonganjatan.taskmanager.model.TaskForm;
+import com.wonganjatan.taskmanager.model.User;
+import com.wonganjatan.taskmanager.service.TaskService;
+import com.wonganjatan.taskmanager.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/user/tasks")
+public class UserTasksController {
+
+    private final TaskService taskService;
+    private final UserService userService;
+
+    @Autowired
+    public UserTasksController(TaskService taskService, UserService userService) {
+        this.taskService = taskService;
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String taskList(
+            Authentication authentication,
+            @RequestParam(name = "priority", required = false) String priority,
+            @RequestParam(name = "status", required = false) String status,
+            @RequestParam(name = "sortBy", required = false) String sortBy,
+            Model model) {
+
+        String username = authentication.getName();
+        Optional<User> userOptional = userService.getUserByUsername(username);
+        User user = userOptional.get();
+
+        Collection<Task> myTasks = taskService.getAllTasksByAssignedUser(user.getId(), priority, status, sortBy);
+        long tasksCount = myTasks.size();
+
+        model.addAttribute("myTasks", myTasks);
+        model.addAttribute("tasksCount", tasksCount);
+        model.addAttribute("selectedPriority", priority);
+        model.addAttribute("selectedStatus", status);
+        model.addAttribute("selectedSortBy", sortBy);
+
+        return "user/tasks/list";
+    }
+
+    @GetMapping("/{id}")
+    public String viewTask(@PathVariable Long id,
+                           Model model,
+                           RedirectAttributes redirectAttributes) {
+
+        Optional<Task> taskOptional = taskService.getTaskById(id);
+
+        if (taskOptional.isEmpty()) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Task is not found");
+            return "redirect:/user/tasks";
+        }
+
+        Task task = taskOptional.get();
+        model.addAttribute("task", task);
+
+        return "user/tasks/view";
+    }
+}
