bddeaa56326cd5149df15e3bff402dfde7eaa925
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-19 18:47:52 +1100

    fix: remove spring security config and move password encoder to main application

--- src/main/java/com/wonganjatan/taskmanager/TaskManagerApplication.java
+++ src/main/java/com/wonganjatan/taskmanager/TaskManagerApplication.java
@@ -4,12 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class TaskManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskManagerApplication.class, args);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
