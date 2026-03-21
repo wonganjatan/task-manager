168b569a30a6a716a9477ad86c7d44fa80a50c8c
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-21 20:54:57 +1100

    feat(AdminTasksController): replace Thymeleaf to React REST api

--- src/main/java/com/wonganjatan/taskmanager/controller/admin/AdminTasksController.java
+++ src/main/java/com/wonganjatan/taskmanager/controller/admin/AdminTasksController.java
@@ -7,17 +7,18 @@ import com.wonganjatan.taskmanager.service.TaskService;
 import com.wonganjatan.taskmanager.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
-@Controller
-@RequestMapping("/admin/tasks")
+@RestController
+@RequestMapping("api/admin/tasks")
+@CrossOrigin(origins = "http://localhost:5173")
 public class AdminTasksController {
 
     private final TaskService taskService;
@@ -30,56 +31,29 @@ public class AdminTasksController {
     }
 
     @GetMapping
-    public String taskList(
+    public ResponseEntity<Map<String, Object>> getAllTasks(
             @RequestParam(name = "priority", required = false) String priority,
             @RequestParam(name = "status", required = false) String status,
-            @RequestParam(name = "sortBy", required = false) String sortBy,
-            Model model) {
+            @RequestParam(name = "dueDate", required = false) String dueDate) {
 
-        Collection<Task> allTasks = taskService.getAllTasks(priority, status, sortBy);
-        long tasksCount = allTasks.size();
+        Map<String, Object> response = new HashMap<>();
+        Collection<Task> tasks = taskService.getAllTasks(priority, status, dueDate);
+        long totalTasks = tasks.size();
+        response.put("tasks", tasks);
+        response.put("totalTasks", totalTasks);
 
-        model.addAttribute("allTasks", allTasks);
-        model.addAttribute("tasksCount", tasksCount);
-        model.addAttribute("selectedPriority", priority);
-        model.addAttribute("selectedStatus", status);
-        model.addAttribute("selectedSortBy", sortBy);
-
-        return "admin/tasks/list";
+        return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/new")
-    public String showTaskCreationForm(Model model) {
-
-        if (!model.containsAttribute("taskForm")) {
-            model.addAttribute("taskForm", new TaskForm());
-        }
-
-        Collection<User> users = userService.getAllUsers();
-
-        model.addAttribute("pageTitle", "Create Task");
-        model.addAttribute("users", users);
-        model.addAttribute("formSubmitButtonLabel", "Create Task");
-
-        return "admin/tasks/form";
-    }
-
-    @PostMapping
-    public String createTask(@Valid @ModelAttribute("taskForm") TaskForm form,
-                             BindingResult result,
-                             Model model,
-                             RedirectAttributes redirectAttributes) {
-
-        if (result.hasErrors()){
-            return "admin/tasks/form";
-        }
+    @PostMapping("/new")
+    public ResponseEntity<Map<String, String>> createTask(@Valid @RequestBody TaskForm form) {
 
         Task newTask = new Task();
         newTask.setTitle(form.getTitle());
         newTask.setDescription(form.getDescription());
         newTask.setPriority(form.getPriority());
         newTask.setStatus(form.getStatus());
-        newTask.setDueDate(form.getDueDate());
+        System.out.println("Form: " + form.getAssignedUserId());
         if (form.getAssignedUserId() != null) {
             Optional<User> assignedUserOptional = userService.getUserById(form.getAssignedUserId());
             User assignedUser = assignedUserOptional.get();
@@ -87,72 +61,42 @@ public class AdminTasksController {
         } else {
             newTask.setAssignedUser(null);
         }
+        newTask.setDueDate(form.getDueDate());
+
+        System.out.println(newTask.getAssignedUser());
 
         try {
             taskService.saveTask(newTask);
-            redirectAttributes.addFlashAttribute("successMessage", "Task is successfully created");
-            return "redirect:/admin/tasks";
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(Map.of("message", "Task created successfully"));
         } catch (IllegalArgumentException e) {
-            model.addAttribute("dateTimeError", e.getMessage());
-            return "admin/tasks/form";
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(Map.of("error", e.getMessage()));
         }
     }
 
     @GetMapping("/{id}")
-    public String viewTask(@PathVariable Long id,
-                           Model model,
-                           RedirectAttributes redirectAttributes) {
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
 
         Optional<Task> taskOptional = taskService.getTaskById(id);
 
         if (taskOptional.isEmpty()) {
-            redirectAttributes.addFlashAttribute("errorMessage", "Task is not found");
-            return "redirect:/admin/tasks";
+            return ResponseEntity.notFound().build();
         }
 
         Task task = taskOptional.get();
-        model.addAttribute("task", task);
 
-        return "admin/tasks/view";
+        return ResponseEntity.ok(task);
     }
 
-    @GetMapping("/{id}/edit")
-    public String showEditTaskForm(@PathVariable Long id,
-                           Model model) {
+    @PutMapping("/{id}")
+    public ResponseEntity<Map<String, String>> editTask(@PathVariable Long id,
+                           @Valid @RequestBody TaskForm form) {
 
-        Collection<User> users = userService.getAllUsers();
         Optional<Task> taskOptional = taskService.getTaskById(id);
-        Task task = taskOptional.get();
-
-        TaskForm taskForm = new TaskForm();
-        taskForm.setTitle(task.getTitle());
-        taskForm.setDescription(task.getDescription());
-        taskForm.setPriority(task.getPriority());
-        taskForm.setStatus(task.getStatus());
-        taskForm.setDueDate(task.getDueDate());
-
-        model.addAttribute("taskForm", taskForm);
-        model.addAttribute("taskId", id);
-        model.addAttribute("pageTitle", "Edit Task");
-        model.addAttribute("users", users);
-        model.addAttribute("formSubmitButtonLabel", "Edit Task");
-
-
-        return "admin/tasks/form";
-    }
-
-    @PostMapping("/{id}/edit")
-    public String editTask(@PathVariable Long id,
-                           @Valid @ModelAttribute("taskForm") TaskForm form,
-                           BindingResult result,
-                           Model model,
-                           RedirectAttributes redirectAttributes) {
-
-        if (result.hasErrors()){
-            return "admin/tasks/form";
+        if (taskOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
         }
-
-        Optional<Task> taskOptional = taskService.getTaskById(id);
         Task task = taskOptional.get();
 
         task.setTitle(form.getTitle());
@@ -170,25 +114,22 @@ public class AdminTasksController {
 
         try {
             taskService.saveTask(task);
-            redirectAttributes.addFlashAttribute("successMessage", "Task is successfully edited");
-            return "redirect:/admin/tasks";
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(Map.of("message", "Task edited successfully"));
         } catch (IllegalArgumentException e) {
-            model.addAttribute("dateTimeError", e.getMessage());
-            return "admin/tasks/form";
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(Map.of("error", e.getMessage()));
         }
     }
 
-    @PostMapping("/{id}/delete")
-    public String deleteTask(@PathVariable Long id,
-                             RedirectAttributes redirectAttributes) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
 
         try {
             taskService.deleteTask(id);
-            redirectAttributes.addFlashAttribute("successMessage", "Task is successfully deleted");
+            return ResponseEntity.noContent().build();
         } catch (IllegalArgumentException e) {
-            redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+            return ResponseEntity.notFound().build();
         }
-
-        return "redirect:/admin/tasks";
     }
 }
