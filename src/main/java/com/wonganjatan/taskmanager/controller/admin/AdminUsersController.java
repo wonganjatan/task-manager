2bd317b0df7821b2066880c4cf64eda3eaa1bb68
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-21 20:46:42 +1100

    feat: add AdminUsersController.java

--- src/main/java/com/wonganjatan/taskmanager/controller/admin/AdminUsersController.java
+++ src/main/java/com/wonganjatan/taskmanager/controller/admin/AdminUsersController.java
@@ -1,4 +1,33 @@
 package com.wonganjatan.taskmanager.controller.admin;
 
+import com.wonganjatan.taskmanager.model.User;
+import com.wonganjatan.taskmanager.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/api/admin/users")
+@CrossOrigin(origins = "http://localhost:5173")
 public class AdminUsersController {
+
+    private final UserService userService;
+
+    @Autowired
+    public AdminUsersController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Collection<User>> getAllUsers() {
+
+        Collection<User> users = userService.getAllUsers();
+
+        return ResponseEntity.ok(users);
+    }
 }
