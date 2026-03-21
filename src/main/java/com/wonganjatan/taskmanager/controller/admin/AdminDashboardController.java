8a8db09b9401252a4e8f1e8e2da077ef7a92a2d0
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-21 20:53:09 +1100

    feat(AdminDashboardController): replace Thymeleaf to React REST api

--- src/main/java/com/wonganjatan/taskmanager/controller/admin/AdminDashboardController.java
+++ src/main/java/com/wonganjatan/taskmanager/controller/admin/AdminDashboardController.java
@@ -2,12 +2,14 @@ package com.wonganjatan.taskmanager.controller.admin;
 
 import com.wonganjatan.taskmanager.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/admin")
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/admin")
+@CrossOrigin(origins = "http://localhost:5173")
 public class AdminDashboardController {
 
     private final TaskService taskService;
@@ -17,13 +19,11 @@ public class AdminDashboardController {
         this.taskService = taskService;
     }
 
-    @GetMapping
-    public String dashboard(Model model) {
+    @GetMapping("/dashboard")
+    public ResponseEntity<Map<String, Long>> getTasksCount() {
 
-        long taskCount = taskService.getTaskCount();
+        long tasksCount = taskService.getTaskCount();
 
-        model.addAttribute("taskCount", taskCount);
-
-        return "admin/dashboard";
+        return ResponseEntity.ok(Map.of("tasksCount", tasksCount));
     }
 }
