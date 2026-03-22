b017f94b5fa0f7469572662b6dd1b3a3d771117a
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-22 13:01:59 +1100

    feat: add JWT aunthetication to admind ashboard

--- src/main/java/com/wonganjatan/taskmanager/controller/admin/AdminDashboardController.java
+++ src/main/java/com/wonganjatan/taskmanager/controller/admin/AdminDashboardController.java
@@ -1,29 +1,43 @@
 package com.wonganjatan.taskmanager.controller.admin;
 
+import com.wonganjatan.taskmanager.model.AdminDashboardResponse;
+import com.wonganjatan.taskmanager.service.JwtService;
 import com.wonganjatan.taskmanager.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/admin")
 @CrossOrigin(origins = "http://localhost:5173")
 public class AdminDashboardController {
 
+    private final JwtService jwtService;
     private final TaskService taskService;
 
     @Autowired
-    public AdminDashboardController(TaskService taskService) {
+    public AdminDashboardController( JwtService jwtService, TaskService taskService) {
+        this.jwtService = jwtService;
         this.taskService = taskService;
     }
 
     @GetMapping("/dashboard")
-    public ResponseEntity<Map<String, Long>> getTasksCount() {
+    public ResponseEntity<AdminDashboardResponse> getTotalTasks(@RequestHeader("Authorization") String token) {
+        try {
+            String role = jwtService.getRoleFromToken(token);
+            if (!role.equals("ADMIN")) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                        .body(new AdminDashboardResponse(
+                                "You are not allowed to access this resource"));
+            }
 
-        long tasksCount = taskService.getTaskCount();
+            long totalTasks = taskService.getTotalTasks();
 
-        return ResponseEntity.ok(Map.of("tasksCount", tasksCount));
+            return ResponseEntity.ok(new AdminDashboardResponse(totalTasks));
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(new AdminDashboardResponse(e.getMessage()));
+        }
     }
 }
---
+++ src/main/java/com/wonganjatan/taskmanager/model/AdminDashboardResponse.java
@@ -0,0 +1,23 @@
+package com.wonganjatan.taskmanager.model;
+
+public class AdminDashboardResponse {
+
+    private Long totalTasks;
+    private String message;
+
+    public AdminDashboardResponse(Long totalTasks) {
+        this.totalTasks = totalTasks;
+    }
+
+    public  AdminDashboardResponse(String message) {
+        this.message = message;
+    }
+
+    // Setter
+    public void setTotalTasks(Long totalTasks) { this.totalTasks = totalTasks; }
+    public void setMessage(String message) { this.message = message; }
+
+    // Getter
+    public Long getTotalTasks() { return totalTasks; }
+    public String getMessage() { return message; }
+}
