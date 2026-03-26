18acb5b81d66b3b97b1b5b296e7d3c81ec2c88f0
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-26 21:58:35 +1100

    feat: add NotFoundException class

---
+++ src/main/java/com/wonganjatan/taskmanager/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.wonganjatan.taskmanager.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
