500fed33fdcbb258991ccce704043fceefab52a1
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-26 21:58:04 +1100

    feat: add UnauthorizedException class

---
+++ src/main/java/com/wonganjatan/taskmanager/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.wonganjatan.taskmanager.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
